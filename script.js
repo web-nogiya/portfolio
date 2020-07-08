911c25fe5a0f152b22a1506c69825b3f62bb9d96
web-nogiya <67999730+web-nogiya@users.noreply.github.com>
2020-07-08 16:31:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+$(function(){
+    $('a[href^=#]').click(function() {
+       var speed = 1000;
+       var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+       var position = target.offset().top;
+       $('body,html').animate({scrollTop:position}, speed, 'swing');
+       return false;
+    });
+    $exp = $('.ansExplain');
+    $ableList = $('.ableList');
+    $AList = $('.ALists');
+    $('.why').hover(
+        function(){
+          $(this).addClass('hover');
+        },function(){
+          $(this).removeClass('hover');
+    });
+    $('.why').click(function(){
+        if($(this).hasClass('done')){
+
+        $exp.fadeOut(1000);
+        $(this).removeClass('done');
+
+    }else{
+        $exp.fadeIn(1000);
+        $(this).addClass('done');
+        }
+    });
+    
+    $('#listContainer').click(function(){
+        if($ableList.hasClass('done')){
+            $AList.addClass('reList')
+            $ableList.fadeOut(1000);
+            $AList.removeClass('list');
+            $ableList.removeClass('done');
+        }else{
+            $AList.removeClass('reList');
+            $ableList.fadeIn(1000);
+            $AList.addClass('list');
+            $ableList.addClass('done');
+        };
+    })
+});
